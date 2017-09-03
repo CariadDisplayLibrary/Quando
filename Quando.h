420a34eab3be2d960f92162aec483e9c1270abe3
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 22:10:10 +0100

    Initial import

---
+++ Quando.h
@@ -0,0 +1,39 @@
+#ifndef _QUANDO_H
+#define _QUANDO_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t Quando10[];
+    extern const uint8_t Quando11[];
+    extern const uint8_t Quando12[];
+    extern const uint8_t Quando13[];
+    extern const uint8_t Quando14[];
+    extern const uint8_t Quando15[];
+    extern const uint8_t Quando16[];
+    extern const uint8_t Quando17[];
+    extern const uint8_t Quando18[];
+    extern const uint8_t Quando19[];
+    extern const uint8_t Quando20[];
+    extern const uint8_t Quando21[];
+    extern const uint8_t Quando22[];
+    extern const uint8_t Quando23[];
+    extern const uint8_t Quando24[];
+    extern const uint8_t Quando25[];
+    extern const uint8_t Quando26[];
+    extern const uint8_t Quando27[];
+    extern const uint8_t Quando28[];
+    extern const uint8_t Quando29[];
+    extern const uint8_t Quando30[];
+    extern const uint8_t Quando31[];
+    extern const uint8_t Quando32[];
+    extern const uint8_t Quando33[];
+    extern const uint8_t Quando34[];
+    extern const uint8_t Quando35[];
+    extern const uint8_t Quando36[];
+    extern const uint8_t Quando37[];
+    extern const uint8_t Quando38[];
+    extern const uint8_t Quando39[];
+    extern const uint8_t Quando40[];
+};
+#endif
